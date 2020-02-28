725f4602182918245a273c4e203c1601b88f3b87
Laercio Ferracini <you@example.com>
2020-02-28 01:16:52 -0300

    First commit tutorial kotlin

---
+++ src/Calculations.kt
@@ -0,0 +1,15 @@
+/**
+ * @project = kotlin-tutorial-jvm
+ *
+ * @author lferracini
+ * @since <pre>28/02/2020</pre>
+ */
+fun sum(a: Int, b: Int): Int {
+    return a + b;
+}
+
+fun anotherSum(a: Int, b: Int) = a + b
+
+fun printSum(a: Int, b: Int): Unit {
+    println("Sum of $a and $b is ${a + b}")
+}
---
+++ src/app.kt
@@ -0,0 +1,16 @@
+/**
+ * @project = kotlin-tutorial-jvm
+ *
+ * @author lferracini
+ * @since <pre>28/02/2020</pre>
+ */
+
+fun main() {
+    println("Tutorial JVM")
+    var sum = sum(1, 6)
+    println(sum)
+    sum = anotherSum(3, 4)
+    println(sum)
+    printSum(5, 2)
+
+}
